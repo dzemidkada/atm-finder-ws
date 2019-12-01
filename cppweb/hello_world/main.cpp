5501cd4de2d9cacf169665084b7404212ac9a7f6
Dzmitry Dzemidka <dimad-prog@mail.ru>
2019-12-01 14:06:01 +0300

    Added some details (image, js, css)

--- cppweb/hello_world/main.cpp
+++ cppweb/hello_world/main.cpp
@@ -1,11 +1,58 @@
 #include "crow_all.h"
 
+
+void send_file(crow::response& res, std::string filename, std::string content_type) {
+    std::ifstream in("../public/" + filename, std::ifstream::in);
+    if (in) {
+        std::ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        res.set_header("Content-Type", content_type);
+        res.write(contents.str());
+    } else {
+        res.code = 404;
+        res.write("Not found");
+    }
+    res.end();
+}
+
+void send_html(crow::response& res, std::string filename) {
+    send_file(res, filename + ".html", "text/html");
+}
+
+void send_image(crow::response& res, std::string filename) {
+    send_file(res, "images/" + filename, "image/jpeg");
+}
+
+void send_script(crow::response& res, std::string filename) {
+    send_file(res, "scripts/" + filename, "text/javascript");
+}
+
+void send_style(crow::response& res, std::string filename) {
+    send_file(res, "styles/" + filename, "text/css");
+}
+
 int main(int argc, char* argv[]) {
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")
-        ([](){
-            return "<div><h1>Hello world!</h1></div>";
+        ([](const crow::request& req, crow::response& res){
+            send_html(res, "index");
+        });
+
+    CROW_ROUTE(app, "/styles/<string>")
+        ([](const crow::request& req, crow::response& res, std::string filename){
+            send_style(res, filename);
+        });
+
+    CROW_ROUTE(app, "/scripts/<string>")
+        ([](const crow::request& req, crow::response& res, std::string filename){
+            send_script(res, filename);
+        });
+
+    CROW_ROUTE(app, "/images/<string>")
+        ([](const crow::request& req, crow::response& res, std::string filename){
+            send_image(res, filename);
         });
 
     char* port = getenv("PORT");
---
+++ run_server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dir=$(pwd)
+server=/usr/src/cppweb/hello_world/build/hello_world
+docker run -v "$dir"/cppweb:/usr/src/cppweb -p 8888:8888 -e PORT=8888 hello_world:latest "$server"
