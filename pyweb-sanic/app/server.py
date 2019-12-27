65b4fd8ca49c302744107fa34015c2d719de5142
Dzmitry Dzemidka <Dzmitry_Dzemidka@epam.com>
2019-12-27 16:02:49 +0300

    Sanic hello-world

---
+++ pyweb-sanic/app/server.py
@@ -0,0 +1,13 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+
+@app.route("/hello-world")
+async def hello_world(request):
+    return json({"message": "Hello world!"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=False, access_log=False)
--- tank/run_tank.sh
+++ tank/run_tank.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run ^
-    -v C:\Users\Dzmitry_Dzemidka\Desktop\EPAM_Projects\uni\atm-finder-ws\cppweb\tank:/var/loadtest ^
+    -v C:\Users\Dzmitry_Dzemidka\Desktop\EPAM_Projects\uni\atm-finder-ws\tank:/var/loadtest ^
     -v 8080:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent ^
     --net host ^
     -it direvius/yandex-tank
