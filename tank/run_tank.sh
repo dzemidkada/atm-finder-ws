c622697c5cd1ce0ed0b009611414a5677b8db3c3
Dzmitry Dzemidka <dimad-prog@mail.ru>
2019-12-28 22:57:06 +0300

    tmp

--- tank/run_tank.sh
+++ tank/run_tank.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker run ^
-    -v C:\Users\Dzmitry_Dzemidka\Desktop\EPAM_Projects\uni\atm-finder-ws\tank:/var/loadtest ^
-    -v 8080:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent ^
-    --net host ^
+docker run \
+    -v $(pwd):/var/loadtest \
+    -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
+    --net host \
     -it direvius/yandex-tank
